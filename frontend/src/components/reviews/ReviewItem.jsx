c3f0e2fe7129cb6598a90c92cb0ee4dc41b2d42d
Denis Krasnokutsky <npp_everest@inbox.ru>
2023-02-22 18:44:44 +0300

    адаптив - отзывы

--- frontend/src/components/reviews/ReviewItem.jsx
+++ frontend/src/components/reviews/ReviewItem.jsx
@@ -6,16 +6,16 @@ const ReviewItem = ({ id, title, photo, content, dateReview }) => {
         <>
                 <div className="card mb-3" key={id}>
                         <div className="row">
-                            <div className="col-xxl-4">
+                            <div className="col-xxl-4 col-xl-4">
                                 <img 
                                     src={photo} 
                                     className="reviews__photo z-depth-1" 
                                 />
                             </div>
-                            <div className="col-xxl-8">
+                            <div className="col-xxl-8 col-xl-8">
                                 <div className="card-body">
                                       <h3 className="card-title">{title}</h3>
-                                      <div className="card-text" dangerouslySetInnerHTML={{ __html: content.slice(0,600) }}/>
+                                      <div className="card-text content" dangerouslySetInnerHTML={{ __html: content.slice(0,500) }}/>
                                       <p className="card-text"><small className="text-muted">{dateReview}</small></p>
                                 </div>
                             </div>
